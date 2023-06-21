d4a69400fddbfb1238e919428ebae7f34975372b
s13bp <sahibbharajparminder@gmail.com>
2023-06-21 05:02:46 +0300

    fg

---
+++ precache-manifest.7ae772299eacbb1b6944759f3a26967a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e45c671f1f12fe4a6ae3",
+    "url": "/static/css/main.41d520c1.chunk.css"
+  },
+  {
+    "revision": "e45c671f1f12fe4a6ae3",
+    "url": "/static/js/main.4ae22325.chunk.js"
+  },
+  {
+    "revision": "3899a156901277895139",
+    "url": "/static/js/runtime~main.00b5c662.js"
+  },
+  {
+    "revision": "4b6f801ee78ba7e8c990",
+    "url": "/static/js/2.6d170b68.chunk.js"
+  },
+  {
+    "revision": "981a2159f374068c1267",
+    "url": "/static/js/3.cc7db0be.chunk.js"
+  },
+  {
+    "revision": "e2baa80db526e3ff8468efec825bbbe0",
+    "url": "/index.html"
+  }
+];
